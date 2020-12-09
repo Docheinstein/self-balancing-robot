61db8d32e9e804babd6f5194c90835c3530f10bf
Docheinstein <dottorestefano97@gmail.com>
2020-12-09 16:18:28 +0100

    Using huart alias instead of huart1 in serial.c

--- Core/Src/serial.c
+++ Core/Src/serial.c
@@ -1,11 +1,11 @@
 #include "serial.h"
 #include "stm32l4xx_hal.h"
 
-extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef *huart;
 
 // overriding _putchar of so that printf (of custom printf.h)
 // will print to the serial port (UART)
 void _putchar(char character)
 {
-	HAL_UART_Transmit(&huart1, (uint8_t *) &character, 1, HAL_MAX_DELAY);
+	HAL_UART_Transmit(huart, (uint8_t *) &character, 1, HAL_MAX_DELAY);
 }
