c55b0e4f33a780ee4c9d187a2f5116f416f746f0
Docheinstein <dottorestefano97@gmail.com>
2021-04-03 11:15:56 +0200

    Implement serial read (for debug purposes)

--- Core/Inc/serial.h
+++ Core/Inc/serial.h
@@ -1,6 +1,7 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
+#include <stdbool.h>
 #include "stm32l4xx_hal.h"
 #include "printf.h"
 
@@ -9,5 +10,8 @@
 #define println(message, ...) printf(message SERIAL_ENDL, ##__VA_ARGS__)
 
 void Serial_Init(UART_HandleTypeDef *huart);
+bool Serial_Write(uint8_t *data, uint16_t size);
+bool Serial_Read(uint8_t *data, uint16_t size);
+bool Serial_ReadStringCR(char *data, uint16_t size);
 
 #endif /* SERIAL_H */
--- Core/Src/serial.c
+++ Core/Src/serial.c
@@ -1,15 +1,56 @@
 #include "serial.h"
+#include "stm32l4xx_hal_uart.h"
 
 static UART_HandleTypeDef *huart;
 
-void Serial_Init(UART_HandleTypeDef *h) {
+void Serial_Init(UART_HandleTypeDef *h)
+{
 	huart = h;
 }
 
+bool Serial_Write(uint8_t *data, uint16_t size)
+{
+	return HAL_UART_Transmit(huart, data, size, HAL_MAX_DELAY) == HAL_OK;
+}
 
-// overriding _putchar of so that printf (of custom printf.h)
-// will print to the serial port (UART)
+bool Serial_Read(uint8_t *data, uint16_t size)
+{
+	return HAL_UART_Receive(huart, data, size, HAL_MAX_DELAY) == HAL_OK;
+}
+
+
+/*
+ * Reads a '\r' terminated string from UART.
+ */
+bool Serial_ReadStringCR(char *data, uint16_t size)
+{
+	uint16_t c;
+	size = size - sizeof('\0');
+
+	for (c = 0; c < size; c++) {
+		if (HAL_UART_Receive(huart, (uint8_t *) &data[c], 1, HAL_MAX_DELAY) != HAL_OK)
+			return false;
+		if (data[c] == '\r') {
+			data[c] = '\0';
+			break;
+		}
+	}
+
+	return c < size;
+}
+
+/*
+ * Overriding _putchar of so that printf (of custom printf.h)
+ * will print to the serial port (UART)
+ */
 void _putchar(char character)
 {
 	HAL_UART_Transmit(huart, (uint8_t *) &character, 1, HAL_MAX_DELAY);
 }
+
+// TODO: for ReadAsync
+//void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+//{
+//	println("HAL_UART_RxCpltCallback");
+//}
+
