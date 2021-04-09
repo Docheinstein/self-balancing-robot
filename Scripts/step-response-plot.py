31e0dd3234a2e5e962a55bd8ee24017c38066310
Docheinstein <dottorestefano97@gmail.com>
2021-04-09 09:33:30 +0200

    Improve step-response-plot.py script

--- Scripts/step-response-plot.py
+++ Scripts/step-response-plot.py
@@ -1,7 +1,8 @@
+from math import ceil
 import argparse
 import sys
+import os
 import matplotlib.pyplot as plt
-import numpy as np
 
 """
 Input data file syntax:
@@ -12,8 +13,15 @@ TIME2 INPUT2 OUTPUT2
 ...
 """
 
-def do_plot_step_response(t, i, o):
-    fig = plt.figure()
+def zero_centered_range(range_max, range_half_ticks):
+    range_step = ceil(range_max / range_half_ticks)
+    h = [range_step + i * range_step for i in range(range_half_ticks)]
+    l = [-x for x in h]
+    l.reverse()
+    return l + [0] + h
+    
+def do_plot_step_response(t, i, o, degrees, name="step response plot"):
+    fig = plt.figure(name)
     ax1 = plt.axes()
     ax2 = ax1.twinx()
     
@@ -25,26 +33,43 @@ def do_plot_step_response(t, i, o):
     ax2.plot(t, i, linestyle="dashed", color="limegreen")
     ax1.axhline(y=0,  linestyle="dotted", color="gray")
     
-    ax1.set_yticks(range(-100, 100 + 1, 10))
-    ax2.set_yticks(range(-90, 90 + 1, 10))
+    r1 = zero_centered_range(100, 10)
+    r2 = zero_centered_range(degrees, 10)
+
+    ax1.set_yticks(r1)
+    ax2.set_yticks(r2)
     
-    ax1.set_ylim(-100, 100)
-    ax2.set_ylim(-90, 90)
+    ax1.set_ylim(r1[0], r1[len(r1) - 1])
+    ax2.set_ylim(r2[0], r2[len(r2) - 1])
+    
     
     fig.tight_layout()
     plt.show()
     
-def plot_step_response(data_file):
+def plot_step_response(data_file, verbose=False, degrees=90):
     try:
-        with open(data_file) as f:
-            t, i, o = zip(*[l.split() for l in f.readlines() 
-                            if not l.startswith("#")])
+        with open(data_file, errors="ignore") as f:
+            data = []
+            t = []
+            i = []
+            o = []
             
-            t = [float(x.strip()) for x in t]
-            i = [float(x.strip()) for x in i]
-            o = [float(x.strip()) for x in o]
-            
-            do_plot_step_response(t, i, o)
+            for l in f.readlines():
+                try:
+                    l = l.strip()
+                    if not l.startswith("#"):
+                        entry = l.split()
+                        if len(entry) == 3:
+                            t.append(float(entry[0]))
+                            i.append(float(entry[1]))
+                            o.append(float(entry[2]))
+                    elif verbose:
+                        print(l)
+                except Exception as e:
+                    print(e, file=sys.stderr)
+                    
+            do_plot_step_response(t, i, o, degrees=degrees, 
+                                  name=os.path.basename(data_file))
     except OSError:
         print(f"Failed to open '{data_file}'", file=sys.stderr)
         
@@ -56,10 +81,21 @@ def main():
     parser.add_argument("data_file", metavar="DATA_FILE",
                         type=str,
                         help="path of the data file")
+    parser.add_argument("-v", "--verbose",                                          
+                        action="store_const", const=True, default=False,            
+                        dest="verbose",                                             
+                        help="print comment lines")
+    parser.add_argument("-d", "--degrees",
+                        type=int, default=90,
+                        dest="degrees",
+                        help=f"set y-axis scale max and min degrees (default is 90)")  
+
     
     args = vars(parser.parse_args())
     data_file = args.get("data_file")
-    plot_step_response(data_file)
+    verbose = args.get("verbose")
+    degrees = args.get("degrees")
+    plot_step_response(data_file, verbose=verbose, degrees=degrees)
     
     
 if __name__ == "__main__":
