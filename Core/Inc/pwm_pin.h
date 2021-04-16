856e38ba55dc4e95435cbdcb5e53a2ae5815b78a
Docheinstein <dottorestefano97@gmail.com>
2021-04-16 15:22:14 +0200

    Add setter/getter of PSC, ARR, CCR and function to set PWM freq at runtime

--- Core/Inc/pwm_pin.h
+++ Core/Inc/pwm_pin.h
@@ -2,21 +2,35 @@
 #define PWM_PIN_H
 
 #include <stddef.h>
+#include <stdbool.h>
 #include "stm32l4xx_hal.h"
 
+
+typedef enum {
+	PWM_PIN_TYPE_16_BIT, // default
+	PWM_PIN_TYPE_32_BIT
+} PWM_PinType;
 
 typedef struct PWM_Pin {
 	TIM_HandleTypeDef *tim;
 	uint32_t channel;
+	PWM_PinType type;
 } PWM_Pin;
 
 void PWM_Pin_Start(PWM_Pin pin);
 void PWM_Pin_Stop(PWM_Pin pin);
+
+void PWM_Pin_Set(PWM_Pin, float frequency, float percentage);
+void PWM_Pin_SetFrequency(PWM_Pin pin, float frequency);
 void PWM_Pin_SetDutyCycle(PWM_Pin pin, float percentage);
 
-// Not strictly necessary, can be configured in .ioc at compile time
-void PWM_Pin_SetPSC(PWM_Pin pin, uint16_t psc);
-void PWM_Pin_SetARR(PWM_Pin pin, uint16_t arr);
+void PWM_Pin_SetPSC(PWM_Pin pin, uint32_t psc);
+void PWM_Pin_SetARR(PWM_Pin pin, uint32_t arr);
+void PWM_Pin_SetCCR(PWM_Pin pin, uint32_t ccr);
+
+uint32_t PWM_Pin_GetPSC(PWM_Pin pin);
+uint32_t PWM_Pin_GetARR(PWM_Pin pin);
+uint32_t PWM_Pin_GetCCR(PWM_Pin pin);
 
 void PWM_Pin_ToString(PWM_Pin pin, char *buf, size_t buflen);
 
--- Core/Src/pwm_pin.c
+++ Core/Src/pwm_pin.c
@@ -26,39 +26,108 @@ void PWM_Pin_Stop(PWM_Pin pin)
 	HAL_TIM_PWM_Stop(pin.tim, pin.channel);
 }
 
+void PWM_Pin_Set(PWM_Pin pin, float frequency, float percentage)
+{
+	PWM_Pin_SetFrequency(pin, frequency);
+	PWM_Pin_SetDutyCycle(pin, percentage);
+}
+
+void PWM_Pin_SetFrequency(PWM_Pin pin, float frequency)
+{
+#if VERBOSE
+	char s_pin[8];
+	PWM_Pin_ToString(pin, s_pin, 8);
+	verboseln("Frequency (%s) := %.2fHz", s_pin, frequency);
+#endif
+
+	/**
+	 * Change the frequency means set the right value of PSC and ARR so that:
+	 * F = CLK / ((PSC + 1) * (ARR + 1))
+	 * (PSC + 1) * (ARR + 1) = CLK / F
+	 *
+	 * Therefore
+	 * (PSC + 1) = (CLK / F) * (1 / (ARR + 1))
+
+	 * PSC       = (CLK / F) * (1 / (ARR + 1)) - 1
+	 */
+
+	float K = SystemCoreClock / frequency;
+	if (frequency > K)
+		return;
+	uint32_t M = pin.type == PWM_PIN_TYPE_32_BIT ? UINT32_MAX : UINT16_MAX;
+
+	// Take the ceil so that the equation yields arr < M
+	uint32_t psc =  ceil((K / (M)) - 1);
+	uint32_t arr = floor((K / (psc + 1))    - 1);
+
+	verboseln("psc=%u | arr=%u | f=%uHz",
+			psc, arr, SystemCoreClock / ((psc + 1) * (arr + 1)));
+
+	PWM_Pin_SetPSC(pin, psc);
+	PWM_Pin_SetARR(pin, arr);
+}
+
 void PWM_Pin_SetDutyCycle(PWM_Pin pin, float percentage)
 {
 #if VERBOSE
 	char s_pin[8];
 	PWM_Pin_ToString(pin, s_pin, 8);
-	verboseln("DutyCycle (%s) := %.2f%%", s_pin, 100 * percentage);
+	verboseln("DutyCycle (%s) := %.2f%%", s_pin, percentage);
 #endif
-	__HAL_TIM_SET_COMPARE(
-			pin.tim, pin.channel,
-			lroundf(__HAL_TIM_GET_AUTORELOAD(pin.tim) * percentage / 100)
+	PWM_Pin_SetCCR(
+			pin,
+			lroundf(PWM_Pin_GetARR(pin) * percentage / 100)
 	);
 }
 
-void PWM_Pin_SetPSC(PWM_Pin pin, uint16_t psc)
+void PWM_Pin_SetPSC(PWM_Pin pin, uint32_t psc)
 {
 #if VERBOSE
 	char s_pin[8];
 	PWM_Pin_ToString(pin, s_pin, 8);
-	verboseln("PSC (%s) := %u%%", s_pin, psc);
+	verboseln("PSC (%s) := %u", s_pin, psc);
 #endif
 	__HAL_TIM_SET_PRESCALER(pin.tim, psc);
 }
 
-void PWM_Pin_SetARR(PWM_Pin pin, uint16_t arr)
+void PWM_Pin_SetARR(PWM_Pin pin, uint32_t arr)
 {
 #if VERBOSE
 	char s_pin[8];
 	PWM_Pin_ToString(pin, s_pin, 8);
-	verboseln("ARR (%s) := %u%%", s_pin, arr);
+	verboseln("ARR (%s) := %u", s_pin, arr);
 #endif
 	__HAL_TIM_SET_AUTORELOAD(pin.tim, arr);
 }
 
+
+void PWM_Pin_SetCCR(PWM_Pin pin, uint32_t ccr)
+{
+#if VERBOSE
+	char s_pin[8];
+	PWM_Pin_ToString(pin, s_pin, 8);
+	verboseln("CCR (%s) := %u", s_pin, ccr);
+#endif
+	__HAL_TIM_SET_COMPARE(pin.tim, pin.channel, ccr);
+}
+
+uint32_t PWM_Pin_GetPSC(PWM_Pin pin)
+{
+	// TODO
+	// Currently there is not __HAL_TIM_GET_PRESCALER in stm32l4xx_hal_tim.h.
+	// Use it if it will ever be added.
+	return pin.tim->Instance->PSC;
+}
+
+uint32_t PWM_Pin_GetARR(PWM_Pin pin)
+{
+	return __HAL_TIM_GET_AUTORELOAD(pin.tim);
+}
+
+uint32_t PWM_Pin_GetCCR(PWM_Pin pin)
+{
+	return __HAL_TIM_GET_COMPARE(pin.tim, pin.channel);
+}
 
 static const char * PWM_Pin_TIMToString(TIM_HandleTypeDef *tim)
 {
