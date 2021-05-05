cdbe59aacaa3d719b0bc9b18b360e14c7c59a9a3
Docheinstein <dottorestefano97@gmail.com>
2021-05-05 14:20:51 +0200

    Minor change of messages printed by pwm params.py

--- Scripts/pwm-params.py
+++ Scripts/pwm-params.py
@@ -3,19 +3,19 @@ from math import ceil
 
 """
                     Fclock
-Fmotor = --------------------------
+Fpwm = --------------------------
             (PSC + 1) * (ARR + 1)
             
 
 =>
                     Fclock
 (ARR + 1) = --------------------------
-                Fmotor * (PSC + 1)
+                Fpwm * (PSC + 1)
                     
 """
 
 
-def compute_params(clock_freq, motor_freq,
+def compute_params(clock_freq, pwm_freq,
                    proposals_max=10):
     UINT16_MAX = 2**16 - 1
     
@@ -23,18 +23,18 @@ def compute_params(clock_freq, motor_freq,
     print("-" * 54)
     print(f"Fclock = {clock_freq}")
     print("-" * 54)
-    print("Fmotor (Hz)".ljust(12) + " | " + "Tmotor (ms)".ljust(12) + 
+    print("Fpwm (Hz)".ljust(12) + " | " + "Tpwm (ms)".ljust(12) + 
             " | " + "PSC".ljust(12) + " | " + "ARR".ljust(12))
     print("-" * 54)
     
     clock_freq = float(clock_freq)
 
     for psc in range(0, UINT16_MAX + 1):
-        arr = ceil((clock_freq / (motor_freq * (psc + 1))) - 1)
+        arr = ceil((clock_freq / (pwm_freq * (psc + 1))) - 1)
         if arr <= UINT16_MAX:
-            motor_freq_real = clock_freq / ((psc + 1) * (arr + 1))
-            period_real = 1000 / motor_freq_real
-            print(f"{motor_freq_real:.2f}".ljust(12) + " | " +
+            pwm_freq_real = clock_freq / ((psc + 1) * (arr + 1))
+            period_real = 1000 / pwm_freq_real
+            print(f"{pwm_freq_real:.2f}".ljust(12) + " | " +
                   f"{period_real:.2f}".ljust(12) + " | " +
                   f"{psc}".ljust(12) + " | " + 
                   f"{arr}".ljust(12))
@@ -47,11 +47,11 @@ def compute_params(clock_freq, motor_freq,
 
 def main():
     parser = argparse.ArgumentParser(description=
-         "Compute PWM parameters for a given frequency of the motor"
+         "Compute PWM parameters for a given frequency"
      )
-    parser.add_argument("motor_freq", metavar="MOTOR_FREQ",
+    parser.add_argument("pwm_freq", metavar="PWM_FREQ",
                         type=float,
-                        help="frequency of the motor (Hz)")
+                        help="frequency of the pwm (Hz)")
     parser.add_argument("--clock-freq", metavar="CLOCK_FREQ", dest="clock_freq",
                         type=float, nargs="?",
                         default=80000000, # 80MHz
@@ -59,8 +59,8 @@ def main():
     
     args = vars(parser.parse_args())
     clock_freq = args.get("clock_freq")
-    motor_freq = args.get("motor_freq")
-    compute_params(clock_freq, motor_freq)
+    pwm_freq = args.get("pwm_freq")
+    compute_params(clock_freq, pwm_freq)
     
 
 if __name__ == "__main__":
