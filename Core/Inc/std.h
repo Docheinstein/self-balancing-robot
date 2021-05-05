58aa34777a9f6930361bb89f352bcf93a4b6ba57
Docheinstein <dottorestefano97@gmail.com>
2021-05-05 14:25:23 +0200

    Move explanation of methods from std.c to std.h

--- Core/Inc/std.h
+++ Core/Inc/std.h
@@ -12,13 +12,26 @@
 #define RAD_TO_DEG(rad) ((rad) * 180.0 / 3.14159265358979323846)
 #define DEG_TO_RAD(deg) ((deg) * 3.14159265358979323846 / 180.0)
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+#define SIGNF(x) ((x) > 0 ? 1.0f : -1.0f)
+
+/* Returns true if s1 == s2 */
 bool streq(const char *s1, const char *s2);
+
+/* Returns true if s1 != s2 */
 bool strneq(const char *s1, const char *s2, size_t len);
+
+/* Returns true if s1 starts with s2. */
 bool strstarts(const char *s1, const char *s2);
 
+/* Maps x from [in_min, in_max] to [out_min, out_max] */
 float mapf(float x, float in_min, float in_max, float out_min, float out_max);
+
+/* Returns max if x > max, min if x < min, x otherwise */
 float rangef(float x, float min, float max);
 
+/* Returns max if x > max, min if x < min, x otherwise */
 int range(int x, int min, int max);
 
 #endif /* STD_H*/
--- Core/Src/std.c
+++ Core/Src/std.c
@@ -11,26 +11,22 @@ bool strneq(const char *s1, const char *s2, size_t len)
 	return strncmp(s1, s2, len) == 0;
 }
 
-/* Returns true if s1 starts with s2. */
 bool strstarts(const char *s1, const char *s2)
 {
 	return strneq(s1, s2, strlen(s2));
 }
 
-/* Maps x from [in_min, in_max] to [out_min, out_max] */
 float mapf(float x, float in_min, float in_max, float out_min, float out_max)
 {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 
-/* Returns max if x > max, min if x < min, x otherwise */
 float rangef(float x, float min, float max)
 {
 	return MIN(max, MAX(min, x));
 }
 
-/* Returns max if x > max, min if x < min, x otherwise */
 int range(int x, int min, int max)
 {
 	return MIN(max, MAX(min, x));
