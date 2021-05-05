00b5f9df13c8b159b5f818b71dbd16ec7caa8f32
Docheinstein <dottorestefano97@gmail.com>
2021-05-05 14:19:02 +0200

    Implement manual StepForward and StepBackward through GPIO in A4988

--- Core/Inc/a4988.h
+++ Core/Inc/a4988.h
@@ -2,26 +2,45 @@
 #define A4988_H
 
 #include "gpio_pin.h"
-#include "pwm_pin.h"
+
+/*
+ * A4988 requires a minimum of us to wait before submit a
+ * falling edge to the step pin; actually it should be about 2us,
+ * but set this a little bit higher to be sure the step is sent correctly.
+ */
+#define A4988_MINIMUM_STEP_PULSE_MICROSECONDS 10
+
+/*
+ * A4988 requires a minimum of milliseconds to wait
+ * after a wake up before use the driver.
+ */
+#define A4988_MINIMUM_STEP_AFTER_WAKE_UP_MILLISECONDS 2
+
+typedef enum {
+	A4988_STEP_DIRECTION_FORWARD,
+	A4988_STEP_DIRECTION_BACKWARD
+} A4988_StepDirection;
 
 typedef struct A4988_Config {
-	PWM_Pin step;
+	GPIO_Pin step;
 	GPIO_Pin direction;
 	GPIO_Pin sleep;
 	bool inverted;
-	uint16_t steps_per_revolution;
 } A4988_Config;
 
 typedef struct A4988 {
 	A4988_Config config;
+	bool enabled;
 } A4988;
 
 
 void A4988_Init(A4988 *a4988, A4988_Config config);
+
 void A4988_Enable(A4988 *a4988);
 void A4988_Disable(A4988 *a4988);
-void A4988_Forward(A4988 *a4988, float rpm);
-void A4988_Backward(A4988 *a4988, float rpm);
 
+void A4988_StepForward(A4988 *a4988, bool wait_falling_edge);
+void A4988_StepBackward(A4988 *a4988, bool wait_falling_edge);
+void A4988_StepFallingEdge(A4988 *a498);
 
 #endif /* A4988_H */
--- Core/Src/a4988.c
+++ Core/Src/a4988.c
@@ -1,5 +1,6 @@
 #include "a4988.h"
 #include "verbose.h"
+#include "utime.h"
 
 #define VERBOSE_FMT(fmt) "{A4988} " fmt
 
@@ -9,53 +10,66 @@ void A4988_Init(A4988 *a4988, A4988_Config config)
 #define BUFSIZE 8
 	char dir[BUFSIZE] = {'\0'};
 	char step[BUFSIZE] = {'\0'};
+	char sleep[BUFSIZE] = {'\0'};
 	GPIO_Pin_ToString(config.direction, dir, BUFSIZE);
-	PWM_Pin_ToString(config.step, step, BUFSIZE);
+	GPIO_Pin_ToString(config.step, step, BUFSIZE);
+	GPIO_Pin_ToString(config.sleep, sleep, BUFSIZE);
 #undef BUFSIZE
-	verboseln(
+	averboseln(
 		"Initialized" SERIAL_ENDL
 		"- direction: %s" SERIAL_ENDL
-		"- step: %s",
+		"- step: %s" SERIAL_ENDL
+		"- sleep: %s"
+		,
 		dir,
-		step
+		step,
+		sleep
 	);
 #endif
 
 	a4988->config = config;
+	a4988->enabled = false;
 }
 
 void A4988_Enable(A4988 *a4988)
 {
-	verboseln("Enabling...");
+	averboseln("Enabling...");
+	a4988->enabled = true;
 	GPIO_Pin_High(a4988->config.sleep);
-	PWM_Pin_Start(a4988->config.step);
 }
 
 void A4988_Disable(A4988 *a4988)
 {
-	verboseln("Disabling...");
+	averboseln("Disabling...");
+	a4988->enabled = false;
 	GPIO_Pin_Low(a4988->config.sleep);
-	PWM_Pin_Stop(a4988->config.step);
 }
 
-static float A4988_RpmToFreq(A4988 *a4988, float rpm)
+void A4988_StepForward(A4988 *a4988, bool wait_falling_edge)
 {
-	return rpm * (a4988->config.steps_per_revolution / 60.0f);
-}
-
-void A4988_Forward(A4988 *a4988, float rpm)
-{
-	float freq = A4988_RpmToFreq(a4988, rpm);
-	verboseln("Forward %.2f rpm (%f Hz)", rpm, freq);
+	averboseln("Forward");
 	GPIO_Pin_Write(a4988->config.direction, a4988->config.inverted);
-	PWM_Pin_Set(a4988->config.step, freq, 50.0f);
+	GPIO_Pin_High(a4988->config.step);
+	if (wait_falling_edge) {
+		UTime_SleepMicroseconds(A4988_MINIMUM_STEP_PULSE_MICROSECONDS);
+		A4988_StepFallingEdge(a4988);
+	}
 }
 
-void A4988_Backward(A4988 *a4988, float rpm)
+void A4988_StepBackward(A4988 *a4988, bool wait_falling_edge)
 {
-	float freq = A4988_RpmToFreq(a4988, rpm);
-	verboseln("Backward %.2f rpm (%f Hz)", rpm, freq);
+	averboseln("Backward");
 	GPIO_Pin_Write(a4988->config.direction, !a4988->config.inverted);
-	PWM_Pin_Set(a4988->config.step, freq, 50.0f);
+	GPIO_Pin_High(a4988->config.step);
+	if (wait_falling_edge) {
+		UTime_SleepMicroseconds(A4988_MINIMUM_STEP_PULSE_MICROSECONDS);
+		A4988_StepFallingEdge(a4988);
+	}
+}
+
+void A4988_StepFallingEdge(A4988 *a4988)
+{
+	averboseln("Falling Edge");
+	GPIO_Pin_Low(a4988->config.step);
 }
 
