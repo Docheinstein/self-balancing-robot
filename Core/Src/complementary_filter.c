75aaa7c948f4433c94c48ccf2a8a650c8fe6a4b1
Giulia <giuli4994@gmail.com>
2021-05-12 17:02:05 +0200

    fix complementary filter bug introduced with latest changes

--- Core/Src/complementary_filter.c
+++ Core/Src/complementary_filter.c
@@ -38,14 +38,17 @@ void ComplementaryFilter_Compute(
 	// Actually pitch and yaw are not computed since not necessary
 	float xl_roll = RAD_TO_DEG(atan2f(xl.y, xl.z));
 	if (isnanf(filter->roll)) {// just first time
-		filter->roll = filter->_.g_roll = xl_roll;
+		filter->roll = xl_roll;
+#if DISPLAY_RAW_ROLL
+		filter->_.g_roll = xl_roll;
+#endif
 	}
 
-	filter->_.g_roll += g.x * filter->_.sample_time;
-	filter->roll = filter->_.alpha * (filter->_.g_roll) +
-				 filter->_.one_minus_alpha * xl_roll;
+	filter->roll = filter->_.alpha * (filter->roll + g.x * filter->_.sample_time) +
+				   filter->_.one_minus_alpha * xl_roll;
 
 #if DISPLAY_RAW_ROLL
+	filter->_.g_roll += g.x * filter->_.sample_time;
 	aprintln("xl_roll = %f | g_roll = %f | filter_roll = %f", xl_roll, filter->_.g_roll, filter->roll);
 #endif
 
